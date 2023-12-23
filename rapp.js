a8e930ccf355f9e53fd1f66d740523220db32ab1
Kyle Plump <kplump1395@gmail.com>
2023-12-22 20:30:13 -0500

    got two sibliings working

--- rapp.js
+++ rapp.js
@@ -1,21 +1,21 @@
 import { parse } from 'node-html-parser';
 
-const pairing = new Map(); // dependencies { signalID: dependencyArray }
-const signalToSigIDMap = new Map(); // { signalTextName: signalID }
-const signalList = new Map()// {signalID: signal itself}
-let gc = {};
-let state = {};
-let geffects = {}
 
 export function RComponent(config) {
-  gc = config;
+  const pairing = new Map(); // dependencies { signalID: dependencyArray }
+  const signalToSigIDMap = new Map(); // { signalTextName: signalID }
+  const signalList = new Map()// {signalID: signal itself}
+  let gc = config;
+  let state = {};
+  const effects = gc.effects();
+
+  let geffects = effects;
+
   state = config.state;
 
   console.log("config: ", config)
   // console.log('hello: ', config.effects().updateCounter())
-  const effects = gc.effects();
   console.log('gc.state: ', gc.state)
-  geffects = effects;
 
   // REFLECTION STUFF --------------
   // for(let [key, value] of Object.entries(geffects)) {
@@ -58,117 +58,119 @@ export function RComponent(config) {
     }
   })
 
-}
+  function sig(prim) {
 
-export function sig(prim) {
-
-  let _val = prim;
-  const id = Math.random();
-
-  function val() {
-    console.log('called', id)
-    return _val;
-  }
-
-  function setVal(newprim) {
-    _val = newprim;
-    console.log('setting called: ', pairing.get(id))
-    // eval deps
-    const currdeps = pairing.get(id)
-    for(let i = 0, len = currdeps.length; i < len; i ++) {
-      const currdep = currdeps[i]
-      currdep();
+    let _val = prim;
+    const id = Math.random();
+  
+    function val() {
+      console.log('called', id)
+      return _val;
     }
-  }
-
-  val.prototype.sigd = id;
-  pairing.set(id, [])
-  window.tester = val
-  return [ val, setVal ]
-}
-
-export function eff(callback, dependencies) {
-  for(let i = 0, len = dependencies.length; i < len; i ++) {
-    const currdep = dependencies[i]
-    const currList = pairing.get(currdep.prototype.sigd);
-    pairing.set(currdep.prototype.sigd, [ ...currList, callback ])
-  }
-}
-
-function createUpdator(child) {
-  console.log("create updator called for: ",child)
-  const data = child.rawAttrs.split('r-data')[1].split("{")[1].split("}")[0].trim();
-  const id = Math.random();
-  if(child.rawAttrs.includes('data-rid')) {
-    //update id (nested component roots?)
-    const i = child.rawAttrs.split('data-rid="')[1].split('"')[0];
-    child.rawAttrs = child.rawAttrs.replaceAll(i, id)
-    console.log('new: ', child.rawAttrs)
-  }
-  else {
-    child.rawAttrs += ` data-rid="${id}"`;
-  }
-  console.log("id: ", id)
-  // get sigd
-  const sigd = signalToSigIDMap.get(data);
-  const signalItself = signalList.get(sigd);
-
-  // add manual updator
-  const updator = () => {
-    // console.log("updater called")
-    const element = document.querySelector(`[data-rid="${id}"]`)
-
-    console.log('updator called', signalItself[0]() )
-    console.log('e;lemetn: ', element, id)
-    element.innerHTML = signalItself[0]();
-  }
-
-  let signaldeps = pairing.get(sigd);
-  if(!signaldeps) return;
-  signaldeps.push(updator);
-  pairing.set(sigd, signaldeps)
-}
-
-function _parse(root) {
-  for(let i = 0; i < root.childNodes.length; i ++) {
-    const child = root.childNodes[i]
-    // console.log('checking child: ', child)
-    if(child.rawAttrs && child.rawAttrs.includes('r-data')) {
-      createUpdator(child)
-    }
-    else if(child.rawAttrs && child.rawAttrs.includes('r-click')) {
-      const data = child.rawAttrs.split('r-click')[1].split("{")[1].split("}")[0].trim();
-      console.log('clickEvent: ', data, geffects)
-
-      if(Object.keys(geffects).includes(data)) {
-        console.log('child: ', geffects[data])
-        const c = function() {
-          geffects[data].bind(gc.state)();
-          // gc.state is updated here
-          console.log('gc: ', gc.state)
-
-          Object.keys(gc.state).forEach((s) => {
-              const signalTextName = s; // create signal w/ provided default
-              const f = signalToSigIDMap.get(signalTextName);
-              const deps = pairing.get(f);
-              const signalItself = signalList.get(f)
-              signalItself[1](gc.state[s])
-              console.log('f: ', deps)
-              for(let i = 0; i < deps.length; i ++) {
-                deps[i]()
-              }
-          })
-        }
-        const fid = (Math.random() + 1).toString(36).substring(7);
-
-        window[`${fid}`] = c;
-        child.rawAttrs += ` onclick="${fid}()"`
+  
+    function setVal(newprim) {
+      _val = newprim;
+      console.log('setting called: ', pairing.get(id))
+      // eval deps
+      const currdeps = pairing.get(id)
+      for(let i = 0, len = currdeps.length; i < len; i ++) {
+        const currdep = currdeps[i]
+        currdep();
       }
     }
-    else {
-      _parse(child)
+  
+    val.prototype.sigd = id;
+    pairing.set(id, [])
+    window.tester = val
+    return [ val, setVal ]
+  }
+  
+  function eff(callback, dependencies) {
+    for(let i = 0, len = dependencies.length; i < len; i ++) {
+      const currdep = dependencies[i]
+      const currList = pairing.get(currdep.prototype.sigd);
+      pairing.set(currdep.prototype.sigd, [ ...currList, callback ])
     }
   }
-
-  return root
+  
+  function createUpdator(child) {
+    console.log("create updator called for: ",child)
+    const data = child.rawAttrs.split('r-data')[1].split("{")[1].split("}")[0].trim();
+    const id = Math.random();
+    if(child.rawAttrs.includes('data-rid')) {
+      //update id (nested component roots?)
+      const i = child.rawAttrs.split('data-rid="')[1].split('"')[0];
+      child.rawAttrs = child.rawAttrs.replaceAll(i, id)
+      console.log('new: ', child.rawAttrs)
+    }
+    else {
+      child.rawAttrs += ` data-rid="${id}"`;
+    }
+    console.log("id: ", id)
+    // get sigd
+    const sigd = signalToSigIDMap.get(data);
+    const signalItself = signalList.get(sigd);
+  
+    // add manual updator
+    const updator = () => {
+      // console.log("updater called")
+      const element = document.querySelector(`[data-rid="${id}"]`)
+  
+      console.log('updator called', signalItself[0]() )
+      console.log('e;lemetn: ', element, id)
+      element.innerHTML = signalItself[0]();
+    }
+  
+    let signaldeps = pairing.get(sigd);
+    if(!signaldeps) return;
+    signaldeps.push(updator);
+    pairing.set(sigd, signaldeps)
+  }
+  
+  function _parse(root) {
+    for(let i = 0; i < root.childNodes.length; i ++) {
+      const child = root.childNodes[i]
+      // console.log('checking child: ', child)
+      if(child.rawAttrs && child.rawAttrs.includes('r-data')) {
+        createUpdator(child)
+      }
+      else if(child.rawAttrs && child.rawAttrs.includes('r-click')) {
+        const data = child.rawAttrs.split('r-click')[1].split("{")[1].split("}")[0].trim();
+        console.log('clickEvent: ', data, geffects)
+  
+        if(Object.keys(geffects).includes(data)) {
+          console.log('child: ', geffects[data])
+          const c = function() {
+            console.log("ge: ", geffects)
+            console.log('gc: ', gc.state)
+  
+            geffects[data].bind(gc.state)();
+            // gc.state is updated here
+  
+            Object.keys(gc.state).forEach((s) => {
+                const signalTextName = s; // create signal w/ provided default
+                const f = signalToSigIDMap.get(signalTextName);
+                const deps = pairing.get(f);
+                const signalItself = signalList.get(f)
+                signalItself[1](gc.state[s])
+                console.log('f: ', deps)
+                for(let i = 0; i < deps.length; i ++) {
+                  deps[i]()
+                }
+            })
+          }
+          const fid = (Math.random() + 1).toString(36).substring(7);
+  
+          window[`${fid}`] = c;
+          child.rawAttrs += ` onclick="${fid}()"`
+        }
+      }
+      else {
+        _parse(child)
+      }
+    }
+  
+    return root
+  }
+  
 }
